a688e36bb91eb31ad09f87dd2e1e03ea632405ae
Franze Angelei Young <120648811+frnzyng@users.noreply.github.com>
2023-05-23 11:08:32 +0800

    Add files via upload

---
+++ exercise5/model/McDonalds.java
@@ -0,0 +1,5 @@
+package model;
+
+public class McDonalds extends Restaurant{
+    
+}
---
+++ exercise5/model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurant {
+
+    private String ratings;
+    private String price;
+
+    public String getRatings() {
+        return this.ratings;
+    }
+
+    public void setRatings(String ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
